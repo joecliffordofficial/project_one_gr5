0f683909fe8b711a068d77b4b7e256a8b87c6931
Joseph Clifford <joecliffordofficial@gmail.com>
2021-10-23 10:28:47 -0700

    fixes to API script

--- assets/logic.js
+++ assets/logic.js
@@ -1,36 +1,47 @@
-var userInputTasty = $("user-input").val()
-var userInputDrink = $("user-input2").val()
+var userInput = $("user-input").val()
 
-// Need to change userOutputDrink to a dropdown 
+function foodSearch() {
 
-fetch(`https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=${userInputTasty}`, {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "tasty.p.rapidapi.com",
-        "x-rapidapi-key": "5894992dbamsh2db1d89edad00e1p1111dcjsnc598ed0d5cb0"
-    }
-})
-    .then(response => {
-        console.log(response);
-        return response.json()
+    fetch(`https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=${userInput}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "tasty.p.rapidapi.com",
+            "x-rapidapi-key": "5894992dbamsh2db1d89edad00e1p1111dcjsnc598ed0d5cb0"
+        }
     })
-    .then(function (data) {
-        console.log(data)
-    })
-    .catch(err => {
-        console.error(err);
-    });
+        .then(response => {
+            console.log(response);
+            if (response.status !== 200) {
+                alert("Error, link is broken")
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    giphySearch()
+};
 
-fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?i=${userInputDrink}`, {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-        "x-rapidapi-key": "5894992dbamsh2db1d89edad00e1p1111dcjsnc598ed0d5cb0"
-    }
-})
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+function giphySearch() {
+    var giphyKey = "1KNRVra7wA4kaeelDyw7fvxA0q2s7FcC"
+    var giphyURL = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&limi=1&q=${userInput}`
+    fetch(giphyURL)
+        .then(function (response) {
+            if (response.status !== 200) {
+                alert("Error, link is broken")
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(function (data) {
+            console.log(data)
+        });
+}
+
+$("#search-btn").on("click", foodSearch);
