c4b401ab6d15c74f6bdc58acb5cf4ba21fc5ab59
Steven Eiselt <seiselts@gmail.com>
2021-10-28 18:09:27 -0700

    javascript updates

--- assets/logic.js
+++ assets/logic.js
@@ -1,8 +1,9 @@
-var userInput = $("user-input").val()
+var userInput = ""
+var searchNumber = 0
 
 function foodSearch() {
-
-    fetch(`https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=${userInput}`, {
+    userInput = $("#searchBar").val()
+    fetch(`https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&q=${userInput}`, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "tasty.p.rapidapi.com",
@@ -10,39 +11,76 @@ function foodSearch() {
         }
     })
         .then(response => {
-            console.log(response);
             if (response.status !== 200) {
-                alert("Error, link is broken")
+                alert("Error, " + response.status) // CHANGE TO POPUP MODAL!!`
             }
             else {
                 return response.json()
             }
         })
         .then(function (data) {
-            console.log(data)
+            console.log("This is the food search (processed)", data)
+                $(".card-title").text(data.results[searchNumber].name)
+                $(".card-subtitle").text(data.results[searchNumber].description)
+                // Why does this not work?
+                if (data.results[searchNumber].cook_time_minutes > 0) {
+                    $(".dish-cook-time").text("Cook time: " + data.results[searchNumber].cook_time_minutes + " minutes")
+                } 
+                console.log(data.results[searchNumber].instructions[searchNumber].display_text)
+                $(".card-text").empty();
+                for (let i = searchNumber; i < data.results[searchNumber].instructions.length; i++) {
+                    var cookInstructions = $("<li>")
+                    cookInstructions.text(data.results[searchNumber].instructions[i].display_text)
+                    $(".card-text").append(cookInstructions)
+                }
+            giphySearch()
         })
         .catch(err => {
             console.error(err);
         });
-    giphySearch()
 };
 
 function giphySearch() {
+    userInput = $("#searchBar").val()
     var giphyKey = "1KNRVra7wA4kaeelDyw7fvxA0q2s7FcC"
     var giphyURL = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&limi=1&q=${userInput}`
     fetch(giphyURL)
         .then(function (response) {
             if (response.status !== 200) {
-                alert("Error, link is broken")
+                alert("Error, " + response.status)
             }
             else {
                 return response.json()
             }
         })
         .then(function (data) {
-            console.log(data)
+            console.log("This is the processed giphy response data", data)
+            var memeFig = $("<figure>")
+            var memeImg = $("<img>")
+            var memeCap = $("<p>")
+            $("#meme-box").empty(); 
+            memeImg.attr("src", data.data[searchNumber].images.downsized.url)
+            memeFig.append(memeImg,memeCap)
+            $("#meme-box").append(memeFig)
         });
 }
 
-$("#search-btn").on("click", foodSearch);
- console.log(foodSearch)
+function storeRecipe() {
+    console.log("button clicking")
+    localStorage.setItem(
+// needs to store data from within the card-body class
+}
+
+$("#search-btn").on("click", function() {
+    foodSearch();
+});
+
+$("#next-btn").on("click", function(){
+        searchNumber++ 
+        foodSearch();
+});
+
+$("#store-btn").on("click", function() {
+    storeRecipe();
+});
+
